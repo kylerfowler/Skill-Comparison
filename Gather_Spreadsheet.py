b5205acd6304412487b5becbd743c765b05c7082
Justin Kehoe <47858778+JustinKehoe12@users.noreply.github.com>
2019-03-02 20:37:40 -0600

    Bug Fix/Readability
    Fixed weird formatting of categories with one name and also added titles and whitespace for readability.

--- Gather_Spreadsheet.py
+++ Gather_Spreadsheet.py
@@ -20,12 +20,26 @@ i = 2
 while i <= 55:
     stats.append(sheet.row_values(i)[1:])
     # Sleeps to stop google from shitting itself over to many queries per second
-    sleep(.1)
+    sleep(.15)
     i += 1
 
 # Make sure you use the right name here.
 sheet = client.open("Skills-Comparison-Spreadsheet").sheet1
 
 # Convert cell values to a pandas data frame
-dataframe = pd.DataFrame(stats, index=statNames, columns=names)
-print(dataframe)
+df = pd.DataFrame(stats, index=statNames, columns=names)
+
+# Create stat categories
+universal = df.loc["Height_(ft-in)":"Team_Wins", "Joey":"Dan"]
+track = pd.concat([universal.loc["Height_(ft-in)":"Team_Wins", "Joey":"Marc"], df.loc["1st_Place":"Discus_Throw_(ft-in)", "Joey":"Marc"]])
+tennis = pd.concat([universal.loc["Height_(ft-in)":"Team_Wins", "Ryan":"Sam"], df.loc["1st_Place":"3rd_Place", "Ryan":"Sam"], df.loc["Tennis_Aces":"Tennis_Serve_Velocity_(mph)", "Ryan":"Sam"]])
+golf = pd.concat([universal.loc["Height_(ft-in)":"Team_Wins", "Evan":"Evan"], df.loc["1st_Place":"3rd_Place", "Evan":"Evan"], df.loc["Drive_Distance_(ft)":"Average Score", "Evan":"Evan"]])
+baseball = pd.concat([universal.loc["Height_(ft-in)":"Team_Wins", "Kyle":"Kyle"], df.loc["Plate_Appearances":"Changeup_Velocity_(mph)", "Kyle":"Kyle"]])
+volleyball = pd.concat([universal.loc["Height_(ft-in)":"Team_Wins", "Justin":"Dan"], df.loc["Volleyball_Aces":"Volleyball_Serve_Velocity_(mph)", "Justin":"Dan"]])
+# Test Area
+print("Universal:",'\n',universal,'\n')
+print("Track:",'\n',track,'\n')
+print("Tennis:",'\n',tennis,'\n')
+print("Golf:",'\n',golf,'\n')
+print("Baseball:",'\n',baseball,'\n')
+print("Volleyball:",'\n',volleyball)
